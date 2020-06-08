7398613d89b3731f33a8a01a1171a587a96c0a02
SmoothHacker <laglerscott@gmail.com>
2020-06-07 20:04:31 -0500

    Create disassembly loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cstring>
+#include "disassembler.h"
 
-int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
+    if (std::strcmp(argv[1], "-h") == 0 || argc != 2) {
+        std::cout << "  Usage: ./Blue-World.exe binary" << std::endl;
+    }
+
+    FILE *f = fopen(argv[1], "rb");
+    disassembler disass;
+    disass.fileProcessor(f);
+
+    printf("Disassembly for %s\n\n", argv[1]);
+    while (disass.pc < disass.fileSize) {
+        disass.pc += disass.disassembleToConsole();
+    }
+
     return 0;
 }
