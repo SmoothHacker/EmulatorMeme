b03d297f73542d53d42ce183a505948c7367372b
Scott Lagler <laglerscott@gmail.com>
2020-09-15 10:40:49 -0400

    Create CPU Struct and Switch

--- src/BlueWorld.h
+++ src/BlueWorld.h
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <cstring>
 
-class BlueWorld {
-    struct cpu8080{
-        unsigned char a, b, c;
-    };
-};
+typedef struct ConditionCodes {
+    uint8_t z:1;
+    uint8_t s:1;
+    uint8_t p:1;
+    uint8_t cy:1;
+    uint8_t ac:1;
+    uint8_t pad:3;
+} ConditionCodes;
 
+typedef struct State8080 {
+    uint8_t a;
+    uint8_t b;
+    uint8_t c;
+    uint8_t d;
+    uint8_t e;
+    uint8_t h;
+    uint8_t l;
+    uint16_t sp;
+    uint16_t pc;
+    uint8_t *memory;
+    struct ConditionCodes cc;
+    uint8_t int_enable;
+} State8080;
